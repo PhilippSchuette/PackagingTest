a0ce047f77941b507a9fd7da79ec598a2991609a
PhilippSchuette <p.schuette@online.de>
2020-08-19 19:57:59 +0200

    add import statements, rename module.pyi

---
+++ py_src/module1.py
@@ -0,0 +1,10 @@
+# type: ignore
+# This is the first module in my PackingTest repository. Here we have test
+# implementations according to the ground rules set out in the README.
+
+# standard library modules first
+import math
+import os
+
+import matplotlib.pyplot as plt
+import numpy as np
---
+++ py_src/stubs/module1.pyi
@@ -0,0 +1,3 @@
+# This is a stub file for MyPy static type checking of the module `module1`
+
+from typing import List
